5f32f2f130c2338eb5373804849cbaba7f53db15
Jaiganesh <jaiganesh0517@gmail.com>
2026-01-23 14:01:40 +0530

    Implement basic arithmetic operations in Operation.java
    Fix variable names for multiplication and division operations, and add missing import statement.

---
+++ Operation.java
@@ -0,0 +1,19 @@
+// This program will let you know about the operator in java with some operation.
+class Operation {
+      public static void main(String [] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter to Number on that Operation is going to perform");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b; // Addition Operation
+            System.out.println("Addition of this numbers are : "+ sum);
+            int diff = a - b; // Substraction Operation
+            System.out.println("Difference of this numbers are : "+ diff);
+            int diff = a * b; // Multiplication Operation
+            System.out.println("Multiplication of this numbers are : "+ mul);
+            int diff = a / b;  // Division Operation
+            System.out.println("Division of this numbers are : "+ div);
+            int mod = a % b; // Modulo Operation
+            System.out.println("Modulo of this numbers are : "+ mod);
+      }
+}
