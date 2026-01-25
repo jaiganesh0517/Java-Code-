eb5e79e9180c3678dec4aecd2c01ed8f7d6f611f
Jaiganesh <jaiganesh0517@gmail.com>
2026-01-25 13:19:30 +0530

    Enhance FirstProgram.java with comments on variables
    Added comments explaining variable declaration and naming conventions in Java.

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,14 @@
+//Hello there this often the first code that a person should write in order to start there coding journey
 class FirstProgram{
   public static void main(String [] args){
     System.out.println("Hello World");
+    /*We will learn how to define a varible in java
+     This Syntax is used to define variable in java
+     data_type variable_name = varibale value;*/
+     int a = 0;
+    //here int = data type , a= varibale name and 0 = is the value assgin to a.
+    /*Naming Convention 1.Varibale name should not start with a numeric value i.e, 1,2,3...etc
+                        2.Java follow camel case in writing the name of the variable
+                        3.Name should not start with the space.*/
   }
 }
