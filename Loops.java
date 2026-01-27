7ce9b6488e8cd574b75040dbeccde63e8972dbce
Jaiganesh <jaiganesh0517@gmail.com>
2026-01-28 00:58:27 +0530

    Implement loops in Java to print numbers from 1 to 100
    Added a Java program demonstrating loops with for, while, and do-while constructs to print numbers from 1 to 100.

---
+++ Loops.java
@@ -0,0 +1,25 @@
+/* Hello there in this section we will go through the loops in java .
+-->  Application
+     1.When we have to perform a task repeaditilly then we use
+     2.For example if we have to print the numbers from 1 to 10... we can use print statements 10 times but what if i have to print
+       the number from 1 to 1000...then using print statements is not good at all , so we can use loops here to print numbers this most sophisticated
+       way. */
+//for loop -->Printing numbers from 1 to 100
+class Loops{
+  public static void main(String [] args){
+    for(int i = 1; i <= 100; i++){
+      System.out.print(i+" ");
+    }
+    // Similarly we can perform same operation using while loop and do-while loop
+    int i = 1;
+    while(i<=100){
+      System.out.print(i+" ");
+      i++;
+    }
+    //do-while
+    do{
+      System.out.print(i+" ");
+      i++;
+    } while(i<=100);
+  }
+}
